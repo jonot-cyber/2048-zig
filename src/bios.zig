971ff24aaf747f7e36e82fb5bd0ec87cc73cbd92
Joseph Tessitore <josephtessitore@protonmail.com>
2024-10-18 01:12:02 -0400

    Add alignment to the BIOS
    I didn't realize that the input to the decompression function needed to be aligned, which is fixed now

--- src/bios.zig
+++ src/bios.zig
@@ -1,6 +1,6 @@
 /// Decompresses RLE compressed data into video memory
 /// See https://problemkaputt.de/gbatek.htm#biosdecompressionfunctions for information
-pub fn rlUncompReadNormalWrite16Bit(source: [*]const u8, destination: [*]u8) void {
+pub fn rlUncompReadNormalWrite16Bit(source: [*]align(4) const u8, destination: [*]u8) void {
     return asm volatile ("swi #0x15"
         :
         : [source] "{r0}" (source),
