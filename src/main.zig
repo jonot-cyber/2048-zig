9cca29ef00b34e597390b98b9910bee64f8f6cc4
Joseph Tessitore <josephtessitore@protonmail.com>
2024-10-30 16:24:23 -0400

    Add in offsets

--- src/main.zig
+++ src/main.zig
@@ -90,9 +90,8 @@ pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, size
     while (true) {}
 }
 
-const x_offset = 0;
-const y_offset = 0;
-const animate_frames = 8;
+const x_offset = gba.screen_width / 2 - 32 * 2;
+const y_offset = gba.screen_height / 2 - 32 * 2;
 
 var rng = std.rand.DefaultPrng.init(0);
 var rand = rng.random();
@@ -122,10 +121,10 @@ fn animateTiles(work_tiles: *const [16]?tile.WorkTile) void {
         if (work_tile_maybe) |work_tile| {
             const from_coords = indexToCoordinates(work_tile.from);
             const to_coords = indexToCoordinates(i);
-            const from_x: isize = @intCast(from_coords.x * 32);
-            const from_y: isize = @intCast(from_coords.y * 32);
-            const to_x: isize = @intCast(to_coords.x * 32);
-            const to_y: isize = @intCast(to_coords.y * 32);
+            const from_x: isize = @intCast(from_coords.x * 32 + x_offset);
+            const from_y: isize = @intCast(from_coords.y * 32 + y_offset);
+            const to_x: isize = @intCast(to_coords.x * 32 + x_offset);
+            const to_y: isize = @intCast(to_coords.y * 32 + y_offset);
 
             const x_speed = @divFloor(to_x - from_x, animation_speed);
             const y_speed = @divFloor(to_y - from_y, animation_speed);
@@ -142,7 +141,7 @@ fn animateTiles(work_tiles: *const [16]?tile.WorkTile) void {
         }
     }
 
-    for (0..128) |i| {
+    for (obj_i..128) |i| {
         gba.objs[i].set(gba.OBJ{
             .hidden = true,
         });
@@ -165,11 +164,6 @@ fn animateTiles(work_tiles: *const [16]?tile.WorkTile) void {
 }
 
 fn renderTiles(tiles: [16]?u32) void {
-    for (0..128) |i| {
-        gba.objs[i].set(gba.OBJ{
-            .hidden = true,
-        });
-    }
     var idx: usize = 0;
     for (tiles, 0..) |t, i| {
         if (t) |til| {
@@ -177,11 +171,16 @@ fn renderTiles(tiles: [16]?u32) void {
             gba.objs[idx].set(gba.OBJ{
                 .size = .size32,
                 .tile_number = @intCast(til * 16),
-                .x = @intCast(coords.x * 32),
-                .y = @intCast(coords.y * 32),
+                .x = @intCast(coords.x * 32 + x_offset),
+                .y = @intCast(coords.y * 32 + y_offset),
             });
             idx += 1;
         }
+    }
+    for (idx..128) |i| {
+        gba.objs[i].set(gba.OBJ{
+            .hidden = true,
+        });
     }
 }
 
@@ -193,13 +192,58 @@ fn tilesFromWorkTiles(work_tiles: *const [16]?tile.WorkTile) [16]?u32 {
     return ret;
 }
 
+fn setupBackground() void {
+    gba.reg_bg0cnt.* = .{
+        .tile_data = 0,
+        .map_data = 1,
+        .screen_size = .size256x256,
+    };
+    const base_idx = 7 + 32 * 2;
+    for (0..4) |y| {
+        for (0..4) |x| {
+            gba.bg_map[1][base_idx + 0 + 4 * x + 32 * 4 * y].tile_number = 1;
+            gba.bg_map[1][base_idx + 1 + 4 * x + 32 * 4 * y].tile_number = 2;
+            gba.bg_map[1][base_idx + 2 + 4 * x + 32 * 4 * y].tile_number = 2;
+            gba.bg_map[1][base_idx + 3 + 4 * x + 32 * 4 * y].tile_number = 1;
+            gba.bg_map[1][base_idx + 3 + 4 * x + 32 * 4 * y].hflip = true;
+
+            gba.bg_map[1][base_idx + 32 + 4 * x + 32 * 4 * y].tile_number = 3;
+            gba.bg_map[1][base_idx + 33 + 4 * x + 32 * 4 * y].tile_number = 4;
+            gba.bg_map[1][base_idx + 34 + 4 * x + 32 * 4 * y].tile_number = 4;
+            gba.bg_map[1][base_idx + 35 + 4 * x + 32 * 4 * y].tile_number = 3;
+            gba.bg_map[1][base_idx + 35 + 4 * x + 32 * 4 * y].hflip = true;
+
+            gba.bg_map[1][base_idx + 64 + 4 * x + 32 * 4 * y].tile_number = 3;
+            gba.bg_map[1][base_idx + 65 + 4 * x + 32 * 4 * y].tile_number = 4;
+            gba.bg_map[1][base_idx + 66 + 4 * x + 32 * 4 * y].tile_number = 4;
+            gba.bg_map[1][base_idx + 67 + 4 * x + 32 * 4 * y].tile_number = 3;
+            gba.bg_map[1][base_idx + 67 + 4 * x + 32 * 4 * y].hflip = true;
+
+            gba.bg_map[1][base_idx + 96 + 4 * x + 32 * 4 * y].tile_number = 1;
+            gba.bg_map[1][base_idx + 96 + 4 * x + 32 * 4 * y].vflip = true;
+            gba.bg_map[1][base_idx + 97 + 4 * x + 32 * 4 * y].tile_number = 2;
+            gba.bg_map[1][base_idx + 97 + 4 * x + 32 * 4 * y].vflip = true;
+            gba.bg_map[1][base_idx + 98 + 4 * x + 32 * 4 * y].tile_number = 2;
+            gba.bg_map[1][base_idx + 98 + 4 * x + 32 * 4 * y].vflip = true;
+            gba.bg_map[1][base_idx + 99 + 4 * x + 32 * 4 * y].tile_number = 1;
+            gba.bg_map[1][base_idx + 99 + 4 * x + 32 * 4 * y].vflip = true;
+            gba.bg_map[1][base_idx + 99 + 4 * x + 32 * 4 * y].hflip = true;
+        }
+    }
+}
+
 export fn main() noreturn {
     gba.copyPalette(tiles_img.palette, &gba.obj_palettes[0]);
     gba.copyTiles(tiles_img.tiles[0..], gba.obj_tiles[0..]);
+
+    gba.copyPalette(bg_img.palette, &gba.bg_palettes[0]);
+    gba.copyTiles(bg_img.tiles[0..], gba.bg_tiles[0..]);
     gba.reg_dispcnt.* = .{
         .character1d = true,
         .display_obj = true,
+        .display_bg0 = true,
     };
+    setupBackground();
 
     var tiles: [16]?u32 = [1]?u32{null} ** 16;
     tile.addTile(&tiles, rand);
