8f96797e985b17a780455d5416e121b113172649
Joseph Tessitore <josephtessitore@protonmail.com>
2024-10-30 10:26:27 -0400

    Rewrite tile sliding

--- src/main.zig
+++ src/main.zig
@@ -5,382 +5,187 @@ const alphabet = @import("alphabet.zig");
 const bios = @import("bios.zig");
 const compress = @import("compress.zig");
 
+const tile = @import("tile.zig");
+
 const tiles_img = @import("tiles");
 const bg_img = @import("bg");
 
 const _alphabet_letters = alphabet.processLetters(alphabet.letters);
 const alphabet_letters_compressed align(4) = compress.rlCompress(@ptrCast(&_alphabet_letters), @sizeOf(@TypeOf(_alphabet_letters)));
+
+const Console = struct {
+    iy: u32 = 0,
+    ix: u32 = 0,
+
+    const Writer = std.io.Writer(*@This(), error{EndOfBuffer}, appendWrite);
+
+    pub fn init() @This() {
+        gba.reg_dispcnt.display_bg0 = true;
+        gba.copyPalette(.{
+            .{},
+            .{ .r = 31, .g = 31, .b = 31 },
+        } ++ [1]gba.Color{.{}} ** 14, &gba.bg_palettes[0]);
+        var alphabet_letters: [@sizeOf(@TypeOf(_alphabet_letters))]u8 = undefined;
+        bios.rlUncompReadNormalWrite8Bit(@ptrCast(&alphabet_letters_compressed), @ptrCast(&alphabet_letters));
+        bios.bitUnpack(@ptrCast(&alphabet_letters), @ptrCast(gba.bg_tiles[0..]), &.{
+            .zero_data = false,
+            .data_offset = 0,
+            .source_length = @sizeOf(@TypeOf(_alphabet_letters)),
+            .source_unit_width = 1,
+            .destination_unit_width = 4,
+        });
+        gba.reg_bg0cnt.map_data = 2;
+        return .{
+            .ix = 0,
+            .iy = 0,
+        };
+    }
+
+    fn appendWrite(self: *@This(), data: []const u8) error{EndOfBuffer}!usize {
+        const i = self.iy * gba.screen_width + self.ix;
+        if (i + data.len > gba.screen_width * gba.screen_height / 64) {
+            return error.EndOfBuffer;
+        }
+        for (data) |d| {
+            gba.bg_map[2][self.iy * 32 + self.ix].tile_number = d;
+            self.ix += 1;
+            if (self.ix >= gba.screen_width / 8) {
+                self.ix = 0;
+                self.iy += 1;
+            }
+        }
+        return data.len;
+    }
+
+    pub fn clear(self: *@This()) void {
+        for (0..gba.screen_height / 8) |y| {
+            for (0..gba.screen_width / 8) |x| {
+                gba.bg_map[2][y * 32 + x].tile_number = 0;
+            }
+        }
+        self.ix = 0;
+        self.iy = 0;
+    }
+
+    pub fn writer(self: *@This()) @This().Writer {
+        return .{ .context = self };
+    }
+};
+
 pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, size: ?usize) noreturn {
     @setCold(true);
     if (builtin.mode == .ReleaseSmall or builtin.mode == .ReleaseFast) {
         while (true) {}
     }
 
-    gba.reg_dispcnt.* = .{
-        .video_mode = 0,
-        .display_bg0 = true,
-    };
-    gba.copyPalette(.{
-        .{},
-        .{ .r = 31, .g = 31, .b = 31 },
-    } ++ [1]gba.Color{.{}} ** 14, &gba.bg_palettes[0]);
-    var alphabet_letters: [@sizeOf(@TypeOf(_alphabet_letters))]u8 = undefined;
-    bios.rlUncompReadNormalWrite8Bit(@ptrCast(&alphabet_letters_compressed), @ptrCast(&alphabet_letters));
-    bios.bitUnpack(@ptrCast(&alphabet_letters), @ptrCast(gba.bg_tiles[0..]), &.{
-        .zero_data = false,
-        .data_offset = 0,
-        .source_length = @sizeOf(@TypeOf(_alphabet_letters)),
-        .source_unit_width = 1,
-        .destination_unit_width = 4,
-    });
-    gba.reg_bg0cnt.map_data = 2;
-    const PanicWriter = struct {
-        iy: u32 = 0,
-        ix: u32 = 0,
-
-        const Writer = std.io.Writer(
-            *@This(),
-            error{EndOfBuffer},
-            appendWrite,
-        );
-
-        fn appendWrite(
-            self: *@This(),
-            data: []const u8,
-        ) error{EndOfBuffer}!usize {
-            const i = self.iy * gba.screen_width + self.ix;
-            if (i + data.len > gba.screen_width * gba.screen_height / 64) {
-                return error.EndOfBuffer;
-            }
-            for (data) |d| {
-                gba.bg_map[2][self.iy * 32 + self.ix].tile_number = d;
-                self.ix += 1;
-                if (self.ix >= gba.screen_width / 8) {
-                    self.ix = 0;
-                    self.iy += 1;
-                }
-            }
-            return data.len;
-        }
-
-        fn writer(self: *@This()) @This().Writer {
-            return .{ .context = self };
-        }
-    };
-    var pw = PanicWriter{};
-    _ = pw.writer().print("PANIC: {s}\\", .{msg}) catch unreachable;
+    var w = Console.init();
+    _ = w.writer().print("PANIC: {s}\\", .{msg}) catch unreachable;
 
     var it = std.debug.StackIterator.init(@returnAddress(), null);
     while (it.next()) |return_address| {
-        _ = pw.writer().print("T: {x}\\", .{return_address}) catch unreachable;
+        _ = w.writer().print("T: {x}\\", .{return_address}) catch unreachable;
     }
     _ = error_return_trace;
     _ = size;
     while (true) {}
 }
 
-const Tile = struct {
-    number: u4 = 0,
-    obj_i: u7 = 0,
-};
-
-var last_input: gba.Keys = .{};
-var just_pressed: gba.Keys = undefined;
-var just_released: gba.Keys = undefined;
+const x_offset = 0;
+const y_offset = 0;
+const animate_frames = 8;
 
 var rng = std.rand.DefaultPrng.init(0);
 var rand = rng.random();
 
-var used_objects: [128]bool = [1]bool{false} ** 128;
+var last_input: gba.Keys = .{};
 
-fn updateTiles(tiles: [16]?Tile) void {
-    const x_offset = gba.screen_width / 2 - 64;
-    const y_offset = gba.screen_height / 2 - 64;
+fn renderTiles(tiles: [16]?u32) void {
     for (0..128) |i| {
         gba.objs[i].set(gba.OBJ{
             .hidden = true,
         });
     }
-    for (tiles, 0..) |maybe_tile, i| {
-        if (maybe_tile) |tile| {
-            const x: u9 = @intCast(32 * @mod(i, 4) + x_offset);
-            const y: u8 = @intCast(32 * @divFloor(i, 4) + y_offset);
-            gba.objs[tile.obj_i].set(gba.OBJ{
-                .x = x,
-                .y = y,
-                .tile_number = @as(u10, tile.number) * 16,
+    var idx: usize = 0;
+    for (tiles, 0..) |t, i| {
+        if (t) |til| {
+            gba.objs[idx].set(gba.OBJ{
                 .size = .size32,
+                .tile_number = @intCast(til * 16),
+                .x = @intCast((i % 4) * 32),
+                .y = @intCast((i / 4) * 32),
             });
+            idx += 1;
         }
     }
 }
-
-fn moveTile(tiles: *[16]?Tile, x: usize, y: usize, first_x: usize, first_y: usize) void {
-    if (first_x == x and first_y == y) {
-        return;
-    }
-    if (tiles[first_y * 4 + first_x] == null) {
-        tiles[first_y * 4 + first_x] = tiles[y * 4 + x];
-    } else {
-        tiles[first_y * 4 + first_x].?.number += 1;
-        used_objects[tiles[y * 4 + x].?.obj_i] = false;
-    }
-    tiles[y * 4 + x] = null;
-}
-
-const AddTileError = error{
-    NoSpaceAvailable,
-};
-
-fn addTile(tiles: *[16]?Tile) AddTileError!void {
-    const pos = blk: while (true) {
-        const pos = rand.int(u4);
-        if (tiles[pos] != null) {
-            continue;
-        }
-        break :blk pos;
-    };
-    const first_free_obj = blk: for (0..128) |i| {
-        if (!used_objects[i]) {
-            used_objects[i] = true;
-            break :blk i;
-        }
-    } else return error.NoSpaceAvailable;
-    tiles[pos] = Tile{
-        .obj_i = @intCast(first_free_obj),
-        .number = if (rand.int(u2) == 0) 1 else 0,
-    };
-}
-
-fn handleLeft(tiles: *[16]?Tile, just_moved_to: *[16]bool) bool {
-    var moved = false;
-    for (1..4) |x| {
-        for (0..4) |y| {
-            const tile = tiles[y * 4 + x];
-            if (tile == null) {
-                continue;
-            }
-            var ix: i5 = @intCast(x - 1);
-            var first: u4 = @intCast(x);
-            while (ix >= 0) : (ix -= 1) {
-                const nix: u4 = @intCast(ix);
-                if (tiles[y * 4 + nix]) |i_tile| {
-                    if (i_tile.number == tile.?.number and !just_moved_to[y * 4 + nix]) {
-                        just_moved_to[y * 4 + nix] = true;
-                        first = nix;
-                        break;
-                    } else {
-                        first = nix + 1;
-                        break;
-                    }
-                }
-            } else {
-                first = 0;
-            }
-            if (first != x) {
-                moved = true;
-                moveTile(tiles, x, y, first, y);
-            }
-        }
-    }
-    return moved;
-}
-
-fn handleRight(tiles: *[16]?Tile, just_moved_to: *[16]bool) bool {
-    var moved = false;
-    for (1..4) |x_inv| {
-        const x = 3 - x_inv;
-        for (0..4) |y| {
-            const tile = tiles[y * 4 + x];
-            if (tile == null) {
-                continue;
-            }
-            var ix: i5 = @intCast(x + 1);
-            var first: u4 = @intCast(x);
-            while (ix < 4) : (ix += 1) {
-                const nix: u4 = @intCast(ix);
-                if (tiles[y * 4 + nix]) |i_tile| {
-                    if (i_tile.number == tile.?.number and !just_moved_to[y * 4 + nix]) {
-                        just_moved_to[y * 4 + nix] = true;
-                        first = nix;
-                        break;
-                    } else {
-                        first = nix - 1;
-                        break;
-                    }
-                }
-            } else {
-                first = 3;
-            }
-            if (first != x) {
-                moved = true;
-                moveTile(tiles, x, y, first, y);
-            }
-        }
-    }
-    return moved;
-}
-
-fn handleUp(tiles: *[16]?Tile, just_moved_to: *[16]bool) bool {
-    var moved = false;
-    for (1..4) |y| {
-        for (0..4) |x| {
-            const tile = tiles[y * 4 + x];
-            if (tile == null) {
-                continue;
-            }
-            var iy: i5 = @intCast(y - 1);
-            var first: u4 = @intCast(y);
-            while (iy >= 0) : (iy -= 1) {
-                const niy: u4 = @intCast(iy);
-                if (tiles[niy * 4 + x]) |i_tile| {
-                    if (i_tile.number == tile.?.number and !just_moved_to[niy * 4 + x]) {
-                        just_moved_to[niy * 4 + x] = true;
-                        first = niy;
-                        break;
-                    } else {
-                        first = niy + 1;
-                        break;
-                    }
-                }
-            } else {
-                first = 0;
-            }
-            if (first != y) {
-                moved = true;
-                moveTile(tiles, x, y, x, first);
-            }
-        }
-    }
-    return moved;
-}
-
-fn handleDown(tiles: *[16]?Tile, just_moved_to: *[16]bool) bool {
-    var moved = false;
-    for (1..4) |y_inv| {
-        const y = 3 - y_inv;
-        for (0..4) |x| {
-            const tile = tiles[y * 4 + x];
-            if (tile == null) {
-                continue;
-            }
-            var iy: i5 = @intCast(y + 1);
-            var first: u4 = @intCast(y);
-            while (iy < 4) : (iy += 1) {
-                const niy: u4 = @intCast(iy);
-                if (tiles[niy * 4 + x]) |i_tile| {
-                    if (i_tile.number == tile.?.number and !just_moved_to[niy * 4 + x]) {
-                        just_moved_to[niy * 4 + x] = true;
-                        first = niy;
-                        break;
-                    } else {
-                        first = niy - 1;
-                        break;
-                    }
-                }
-            } else {
-                first = 3;
-            }
-            if (first != y) {
-                moved = true;
-                moveTile(tiles, x, y, x, first);
-            }
-        }
-    }
-    return moved;
-}
-
-fn buildBackground() void {
-    const y_origin = gba.screen_height / 16 - 8;
-    const x_origin = gba.screen_width / 16 - 8;
-    for (0..4) |box_y| {
-        for (0..4) |box_x| {
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 0) + (x_origin + box_x * 4 + 0)].tile_number = 1;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 0) + (x_origin + box_x * 4 + 1)].tile_number = 2;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 0) + (x_origin + box_x * 4 + 2)].tile_number = 2;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 0) + (x_origin + box_x * 4 + 3)].tile_number = 1;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 0) + (x_origin + box_x * 4 + 3)].hflip = true;
-
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 1) + (x_origin + box_x * 4 + 0)].tile_number = 3;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 1) + (x_origin + box_x * 4 + 1)].tile_number = 4;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 1) + (x_origin + box_x * 4 + 2)].tile_number = 4;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 1) + (x_origin + box_x * 4 + 3)].tile_number = 3;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 1) + (x_origin + box_x * 4 + 3)].hflip = true;
-
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 2) + (x_origin + box_x * 4 + 0)].tile_number = 3;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 2) + (x_origin + box_x * 4 + 1)].tile_number = 4;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 2) + (x_origin + box_x * 4 + 2)].tile_number = 4;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 2) + (x_origin + box_x * 4 + 3)].tile_number = 3;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 2) + (x_origin + box_x * 4 + 3)].hflip = true;
-
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 3) + (x_origin + box_x * 4 + 0)].tile_number = 1;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 3) + (x_origin + box_x * 4 + 1)].tile_number = 2;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 3) + (x_origin + box_x * 4 + 2)].tile_number = 2;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 3) + (x_origin + box_x * 4 + 3)].tile_number = 1;
-            gba.bg_map[1][32 * (y_origin + box_y * 4 + 3) + (x_origin + box_x * 4 + 3)].hflip = true;
-            for (0..4) |i| {
-                gba.bg_map[1][32 * (y_origin + box_y * 4 + 3) + (x_origin + box_x * 4 + i)].vflip = true;
-            }
-        }
-    }
-    gba.bg_map[1][32 * (gba.screen_height / 16 - 8) + gba.screen_width / 16 - 8].tile_number = 1;
-}
-
-const tiles_palette_compressed align(4) = compress.rlCompress(@ptrCast(&tiles_img.palette), @sizeOf(@TypeOf(tiles_img.palette)));
-const tiles_tiles_compressed align(4) = compress.rlCompress(@ptrCast(&tiles_img.tiles), @sizeOf(@TypeOf(tiles_img.tiles)));
-
-const bg_palette_compressed align(4) = compress.rlCompress(@ptrCast(&bg_img.palette), @sizeOf(@TypeOf(bg_img.palette)));
-const bg_tiles_compressed align(4) = compress.rlCompress(@ptrCast(&bg_img.tiles), @sizeOf(@TypeOf(bg_img.tiles)));
 
 export fn main() noreturn {
-    bios.rlUncompReadNormalWrite16Bit(&tiles_palette_compressed, @ptrCast(&gba.obj_palettes[0]));
-    bios.rlUncompReadNormalWrite16Bit(&tiles_tiles_compressed, @ptrCast(&gba.obj_tiles[0]));
-    bios.rlUncompReadNormalWrite16Bit(&bg_palette_compressed, @ptrCast(&gba.bg_palettes[0]));
-    bios.rlUncompReadNormalWrite16Bit(&bg_tiles_compressed, @ptrCast(&gba.bg_tiles[0]));
+    gba.copyPalette(tiles_img.palette, &gba.obj_palettes[0]);
+    gba.copyTiles(tiles_img.tiles[0..], gba.obj_tiles[0..]);
     gba.reg_dispcnt.* = .{
-        .display_obj = true,
-        .display_bg0 = true,
         .character1d = true,
+        .display_obj = true,
     };
-    gba.reg_bg0cnt.* = .{
-        .tile_data = 0,
-        .map_data = 1,
-    };
-    buildBackground();
-    var tiles = [1]?Tile{null} ** 16;
-    addTile(&tiles) catch unreachable;
-    updateTiles(tiles);
+
+    var tiles: [16]?u32 = [1]?u32{undefined} ** 16;
+    tiles[0] = 0;
+    renderTiles(tiles);
+
+    var animate: ?u32 = null;
     while (true) {
         gba.hBlankWait();
-        const input = gba.reg_keyinput.*;
-        just_pressed = input.justPressed(last_input);
-        just_released = input.justReleased(last_input);
-        last_input = input;
-        var just_moved_to = [1]bool{false} ** 16;
-        const moved = if (!just_pressed.left)
-            handleLeft(&tiles, &just_moved_to)
-        else if (!just_pressed.right)
-            handleRight(&tiles, &just_moved_to)
-        else if (!just_pressed.up)
-            handleUp(&tiles, &just_moved_to)
-        else if (!just_pressed.down)
-            handleDown(&tiles, &just_moved_to)
-        else
-            false;
-        if (moved) {
-            addTile(&tiles) catch unreachable;
-            updateTiles(tiles);
-        }
-        if (!just_pressed.start) {
-            // Reset
-            for (0..16) |i| {
-                if (tiles[i]) |tile| {
-                    used_objects[tile.obj_i] = false;
-                    tiles[i] = null;
+        if (animate) |i| {
+            if (i == 0) {}
+            animate = if (i == 0) null else i - 1;
+        } else {
+            const input = gba.reg_keyinput.*;
+            const just_pressed = input.justPressed(last_input);
+            last_input = input;
+            if (!just_pressed.right) {
+                const work_tiles = tile.slideRight(tiles);
+                for (0..16) |tile_i| {
+                    if (work_tiles[tile_i]) |work_tile| {
+                        tiles[tile_i] = work_tile.value;
+                    } else {
+                        tiles[tile_i] = null;
+                    }
                 }
+                tile.addTile(&tiles, rand);
+                renderTiles(tiles);
+            } else if (!just_pressed.left) {
+                const work_tiles = tile.slideLeft(tiles);
+                for (0..16) |tile_i| {
+                    if (work_tiles[tile_i]) |work_tile| {
+                        tiles[tile_i] = work_tile.value;
+                    } else {
+                        tiles[tile_i] = null;
+                    }
+                }
+                tile.addTile(&tiles, rand);
+                renderTiles(tiles);
+            } else if (!just_pressed.down) {
+                const work_tiles = tile.slideDown(tiles);
+                for (0..16) |tile_i| {
+                    if (work_tiles[tile_i]) |work_tile| {
+                        tiles[tile_i] = work_tile.value;
+                    } else {
+                        tiles[tile_i] = null;
+                    }
+                }
+                tile.addTile(&tiles, rand);
+                renderTiles(tiles);
+            } else if (!just_pressed.up) {
+                const work_tiles = tile.slideUp(tiles);
+                for (0..16) |tile_i| {
+                    if (work_tiles[tile_i]) |work_tile| {
+                        tiles[tile_i] = work_tile.value;
+                    } else {
+                        tiles[tile_i] = null;
+                    }
+                }
+                tile.addTile(&tiles, rand);
+                renderTiles(tiles);
             }
-            addTile(&tiles) catch unreachable;
-            updateTiles(tiles);
         }
     }
 }
---
+++ src/tile.zig
@@ -0,0 +1,155 @@
+const std = @import("std");
+
+const Tiles = [16]?u32;
+
+pub fn addTile(tiles: *[16]?u32, rand: std.rand.Random) void {
+    const free_idx = blk: while (true) {
+        const idx = rand.uintLessThan(usize, tiles.len);
+        if (tiles[idx] == null)
+            break :blk idx;
+    };
+    tiles[free_idx] = if (rand.int(u2) == 0) 1 else 0;
+}
+
+const WorkTile = struct {
+    value: u32,
+    merged: bool,
+    from: usize,
+};
+
+fn slideUnit(tile: u32, row: []const ?WorkTile) usize {
+    for (row, 0..) |r, i| {
+        if (r) |r2| {
+            if (!r2.merged and r2.value == tile) {
+                return i;
+            } else {
+                return i - 1;
+            }
+        }
+    }
+    return row.len - 1;
+}
+
+fn slide(row: *const [4]?u32, from: [4]usize) [4]?WorkTile {
+    var out: [4]?WorkTile = [1]?WorkTile{null} ** 3 ++ [1]?WorkTile{undefined};
+    if (row[3]) |r| {
+        out[3] = .{
+            .value = r,
+            .merged = false,
+            .from = from[3],
+        };
+    }
+    for ([_]usize{ 2, 1, 0 }) |i| {
+        if (row[i]) |r| {
+            const v = slideUnit(r, out[i..]);
+            const merged = if (out[i + v]) |o|
+                o.value == r
+            else
+                false;
+            const q = if (merged) r + 1 else r;
+            out[i + v] = .{
+                .value = q,
+                .merged = merged,
+                .from = from[i],
+            };
+        }
+    }
+    return out;
+}
+
+pub fn slideRight(tiles: [16]?u32) [16]?WorkTile {
+    const wt1 = slide(tiles[0..4], .{ 0, 1, 2, 3 });
+    const wt2 = slide(tiles[4..8], .{ 4, 5, 6, 7 });
+    const wt3 = slide(tiles[8..12], .{ 8, 9, 10, 11 });
+    const wt4 = slide(tiles[12..16], .{ 12, 13, 14, 15 });
+    var out: [16]?WorkTile = undefined;
+    @memcpy(out[0..4], &wt1);
+    @memcpy(out[4..8], &wt2);
+    @memcpy(out[8..12], &wt3);
+    @memcpy(out[12..16], &wt4);
+    return out;
+}
+
+pub fn slideLeft(tiles: [16]?u32) [16]?WorkTile {
+    var tiles_copy = tiles;
+    std.mem.reverse(?u32, tiles_copy[0..4]);
+    var wt1 = slide(tiles_copy[0..4], .{ 3, 2, 1, 0 });
+    std.mem.reverse(?u32, tiles_copy[4..8]);
+    var wt2 = slide(tiles_copy[4..8], .{ 7, 6, 5, 4 });
+    std.mem.reverse(?u32, tiles_copy[8..12]);
+    var wt3 = slide(tiles_copy[8..12], .{ 11, 10, 9, 8 });
+    std.mem.reverse(?u32, tiles_copy[12..16]);
+    var wt4 = slide(tiles_copy[12..16], .{ 15, 14, 13, 12 });
+    var out: [16]?WorkTile = undefined;
+    std.mem.reverse(?WorkTile, &wt1);
+    @memcpy(out[0..4], &wt1);
+    std.mem.reverse(?WorkTile, &wt2);
+    @memcpy(out[4..8], &wt2);
+    std.mem.reverse(?WorkTile, &wt3);
+    @memcpy(out[8..12], &wt3);
+    std.mem.reverse(?WorkTile, &wt4);
+    @memcpy(out[12..16], &wt4);
+    return out;
+}
+
+pub fn slideDown(tiles: [16]?u32) [16]?WorkTile {
+    const col1: [4]?u32 = .{ tiles[0], tiles[4], tiles[8], tiles[12] };
+    const col2: [4]?u32 = .{ tiles[1], tiles[5], tiles[9], tiles[13] };
+    const col3: [4]?u32 = .{ tiles[2], tiles[6], tiles[10], tiles[14] };
+    const col4: [4]?u32 = .{ tiles[3], tiles[7], tiles[11], tiles[15] };
+
+    const wt1 = slide(&col1, .{ 0, 4, 8, 12 });
+    const wt2 = slide(&col2, .{ 1, 5, 9, 13 });
+    const wt3 = slide(&col3, .{ 2, 6, 10, 14 });
+    const wt4 = slide(&col4, .{ 3, 7, 11, 15 });
+
+    var out: [16]?WorkTile = undefined;
+    out[0] = wt1[0];
+    out[4] = wt1[1];
+    out[8] = wt1[2];
+    out[12] = wt1[3];
+    out[1] = wt2[0];
+    out[5] = wt2[1];
+    out[9] = wt2[2];
+    out[13] = wt2[3];
+    out[2] = wt3[0];
+    out[6] = wt3[1];
+    out[10] = wt3[2];
+    out[14] = wt3[3];
+    out[3] = wt4[0];
+    out[7] = wt4[1];
+    out[11] = wt4[2];
+    out[15] = wt4[3];
+    return out;
+}
+
+pub fn slideUp(tiles: [16]?u32) [16]?WorkTile {
+    const col1: [4]?u32 = .{ tiles[12], tiles[8], tiles[4], tiles[0] };
+    const col2: [4]?u32 = .{ tiles[13], tiles[9], tiles[5], tiles[1] };
+    const col3: [4]?u32 = .{ tiles[14], tiles[10], tiles[6], tiles[2] };
+    const col4: [4]?u32 = .{ tiles[15], tiles[11], tiles[7], tiles[3] };
+
+    const wt1 = slide(&col1, .{ 12, 8, 4, 0 });
+    const wt2 = slide(&col2, .{ 13, 9, 5, 1 });
+    const wt3 = slide(&col3, .{ 14, 10, 6, 2 });
+    const wt4 = slide(&col4, .{ 15, 11, 7, 3 });
+
+    var out: [16]?WorkTile = undefined;
+    out[12] = wt1[0];
+    out[8] = wt1[1];
+    out[4] = wt1[2];
+    out[0] = wt1[3];
+    out[13] = wt2[0];
+    out[9] = wt2[1];
+    out[5] = wt2[2];
+    out[1] = wt2[3];
+    out[14] = wt3[0];
+    out[10] = wt3[1];
+    out[6] = wt3[2];
+    out[2] = wt3[3];
+    out[15] = wt4[0];
+    out[11] = wt4[1];
+    out[7] = wt4[2];
+    out[3] = wt4[3];
+    return out;
+}
